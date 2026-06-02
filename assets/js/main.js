74c3aa0b3fe5c9cd1599d402fc8cf3811deea4fa
govindkumawat123 <govindkumawat.github@gmail.com>
2026-06-02 19:16:26 +0530

    Pages added

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,49 @@ $(document).ready(function () {
   $(".ct_close_sidebar").click(function () {
     $("main").removeClass("ct_show");
   });
+
+
+  $(".ct_ads_slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+  $(".ct_drums_slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      991: {
+        items: 2,
+      },
+      1199: {
+        items: 3,
+      },
+      1200: {
+        items: 5,
+      },
+    },
+  });
 });
